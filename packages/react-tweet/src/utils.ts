061880cad5388727eedd40f850cb08cb42494df1
Dan Stroot <dan.stroot@gmail.com>
2026-05-25 09:13:47 -0700

    updated dependencies

--- packages/react-tweet/src/utils.ts
+++ packages/react-tweet/src/utils.ts
@@ -13,7 +13,7 @@ import type {
   MediaVideo,
 } from './api/index.js'
 
-export  { formatDate } from './date-utils.js'
+export { formatDate } from './date-utils.js'
 
 export type TweetCoreProps = {
   id: string
@@ -122,10 +122,11 @@ function getEntities(tweet: TweetBase): Entity[] {
     { indices: tweet.display_text_range, type: 'text' },
   ]
 
-  addEntities(result, 'hashtag', tweet.entities.hashtags)
-  addEntities(result, 'mention', tweet.entities.user_mentions)
-  addEntities(result, 'url', tweet.entities.urls)
-  addEntities(result, 'symbol', tweet.entities.symbols)
+  addEntities(result, 'hashtag', tweet.entities.hashtags ?? [])
+  addEntities(result, 'mention', tweet.entities.user_mentions ?? [])
+  addEntities(result, 'url', tweet.entities.urls ?? [])
+  addEntities(result, 'symbol', tweet.entities.symbols ?? [])
+
   if (tweet.entities.media) {
     addEntities(result, 'media', tweet.entities.media)
   }
@@ -158,7 +159,7 @@ function getEntities(tweet: TweetBase): Entity[] {
 function addEntities(
   result: EntityWithType[],
   type: EntityWithType['type'],
-  entities: TweetEntity[]
+  entities: TweetEntity[] = []
 ) {
   for (const entity of entities) {
     for (const [i, item] of result.entries()) {
